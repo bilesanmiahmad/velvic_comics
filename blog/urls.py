940a54724790901cbf42bb0607387a3bcc92eb75
medobills <fbilesanmi@gmail.com>
2017-05-11 09:19:52 +0000

    views and templates for post detail

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^titles/$', views.post_titles_list, name='post_titles'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
